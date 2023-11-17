bbd9d8b7325d75411f12db7c05daa24406ed1a90
Emhope <michailopalev02@gmail.com>
2023-11-17 09:43:51 +0300

    init

---
+++ Temp/Includes/Program/Typestyp.h
@@ -0,0 +1,39 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1699019033_3_
+#define _BUR_1699019033_3_
+
+#include <bur/plctypes.h>
+
+/* Datatypes and datatypes of function blocks */
+typedef enum doorSt
+{	stINIT,
+	stUNKNOWN,
+	stOPEN,
+	stCLOSE,
+	stACC_POS,
+	stACC_NEG,
+	stPOS,
+	stNEG,
+	stDEC_POS,
+	stDEC_NEG
+} doorSt;
+
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/Program/Types.typ\\\" scope \\\"local\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1699019033_3_ */
+
